8fa0402f8415246083f753d8c4da2eef829cd777
XieTian <Stevens@Stevens-MacBook-Air.local>
2018-03-13 15:34:18 +0800

    🛠：详情页收藏

--- js/pages/RepositoryDetail.js
+++ js/pages/RepositoryDetail.js
@@ -1,3 +1,8 @@
+/**
+ * RepositoryDetail
+ * @flow
+ **/
+'use strict'
 import React, {Component} from 'react'
 import {
     Image,
@@ -11,20 +16,43 @@ import {
 } from 'react-native'
 import NavigationBar from '../common/NavigationBar'
 import ViewUtils from '../util/ViewUtils'
+import FavoriteDao from '../expand/dao/FavoriteDao'
 const TRENDING_URL = 'https://github.com/'
 export default class RepositoryDetail extends Component {
     constructor(props) {
         super(props);
-
         this.url = this.props.projectModel.item.html_url ? this.props.projectModel.item.html_url
             : TRENDING_URL + this.props.projectModel.item.fullName;
         var title = this.props.projectModel.item.full_name ? this.props.projectModel.item.full_name
             : this.props.projectModel.item.fullName;
-
+        this.favoriteDao = new FavoriteDao(this.props.flag);
         this.state = {
             url: this.url,
             canGoBack: false,
             title: title,
+            isFavorite: this.props.projectModel.isFavorite,
+            favoriteIcon: this.props.projectModel.isFavorite ? require('../../res/images/ic_star.png') : require('../../res/images/ic_star_navbar.png'),
+
+        }
+    }
+    componentWillUnmount() {
+        if (this.props.onUpdateFavorite)this.props.onUpdateFavorite();
+    }
+    setFavoriteState(isFavorite) {
+        this.setState({
+            isFavorite: isFavorite,
+            favoriteIcon: isFavorite ? require('../../res/images/ic_star.png') : require('../../res/images/ic_star_navbar.png')
+        })
+    }
+
+    onRightButtonClick() {//favoriteIcon单击回调函数
+        var projectModel = this.props.projectModel;
+        this.setFavoriteState(projectModel.isFavorite = !projectModel.isFavorite);
+        var key = projectModel.item.fullName ? projectModel.item.fullName : projectModel.item.id.toString();
+        if (projectModel.isFavorite) {
+            this.favoriteDao.saveFavoriteItem(key, JSON.stringify(projectModel.item));
+        } else {
+            this.favoriteDao.removeFavoriteItem(key);
         }
     }
 
@@ -42,7 +70,14 @@ export default class RepositoryDetail extends Component {
             url: navState.url,
         });
     }
-
+    renderRightButton() {
+        return <TouchableOpacity
+            onPress={()=>this.onRightButtonClick()}>
+            <Image
+                style={{width: 20, height: 20,marginRight:10}}
+                source={this.state.favoriteIcon}/>
+        </TouchableOpacity>
+    }
     render() {
         return (
             <View style={styles.container}>
@@ -50,7 +85,7 @@ export default class RepositoryDetail extends Component {
                     leftButton={ViewUtils.getLeftButton(()=>this.onBack())}
                     popEnabled={false}
                     title={this.state.title}
-                    rightButton={null}
+                    rightButton={this.renderRightButton()}
                 />
                 <WebView
                     ref={webView=>this.webView=webView}
